b83ab9c713df4bc4156d6339ff4db8d95065b0ab
Daniel <dankrusz@hotmail.co.uk>
2023-09-20 15:42:02 +0100

    Updated after first exercise

--- lib/make_snippet.py
+++ lib/make_snippet.py
@@ -2,6 +2,9 @@
 
 def make_snippet(string):
     list_of_string = string.split()
-    new_string = " ".join(list_of_string[:5])
-    return new_string + "..."
+    if len(list_of_string) > 5:
+        new_string = " ".join(list_of_string[:5])
+        return new_string + "..."
+    else:
+        return string
 
--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -1,6 +1,10 @@
 from lib.make_snippet import make_snippet
-
-def test_make_snippet_correct():
+#tests is string is over 5 words
+def test_make_snippet_correct_over_five():
     result = make_snippet("This should only return words before the before")
-
     assert result == "This should only return words..."
+
+#tests if string is 5 and under
+def test_make_snippet_correct_under_five():
+    result = make_snippet("This should not have dots")
+    assert result == "This should not have dots"
