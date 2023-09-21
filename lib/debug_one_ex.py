4a73d5115e33086c4bd7c00573b817db38195de1
Daniel <dankrusz@hotmail.co.uk>
2023-09-21 11:49:12 +0100

    debugging challenge completed

---
+++ lib/debug_one_ex.py
@@ -0,0 +1,54 @@
+def encode(text, key):
+    cipher = make_cipher(key)
+
+    ciphertext_chars = []
+    print("iterating through the letters in text.")
+    print(cipher)
+    for i in text:
+        ciphered_char = chr(65 + cipher.index(i))
+        ciphertext_chars.append(ciphered_char)
+
+    return "".join(ciphertext_chars)
+
+
+def decode(encrypted, key):
+    cipher = make_cipher(key)
+
+    plaintext_chars = []
+    for i in encrypted:
+        plain_char = cipher[ord(i) - 65] # Originally 65 - ord(i)
+        plaintext_chars.append(plain_char)
+
+    return "".join(plaintext_chars)
+
+
+def make_cipher(key):
+    alphabet = [chr(i + 97) for i in range(0, 26)] # alphabet range was wrong, so was i + 98 
+    cipher_with_duplicates = list(key) + alphabet
+
+    cipher = []
+    for i in range(0, len(cipher_with_duplicates)):
+        if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
+            cipher.append(cipher_with_duplicates[i])
+
+    return cipher
+
+# When you run this file, these next lines will show you the expected
+# and actual outputs of the functions above.
+print(f"""
+Running: encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+Expected: EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL
+Actual: {encode('theswiftfoxjumpedoverthelazydog', 'secretkey')}
+""")
+
+print(f"""
+Running: decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+Expected: theswiftfoxjumpedoverthelazydog
+Actual: {decode('EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL', 'secretkey')}
+""")
+
+#print(f"""
+#Running: encode("t", "secretkey")
+#Expected: E
+#Actual: {encode('t', 'secretkey')}
+#""")
