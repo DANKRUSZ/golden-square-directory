03c5034f1f77be256bc4620e489f7fc785ee761a
David O'Donnell <dave@soundmotives.net>
2023-09-21 16:50:16 +0100

    450pm

--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -1,6 +1,9 @@
 import pytest
-from lib.diary_entry 
+from lib.diary_entry import DiaryEntry
 
-
-def format_is_correct():
-    diaryentry = DiaryEntry()
+def test_format_is_correct():
+    diaryentry = DiaryEntry("Thursday", "I am tired")
+    #diaryentry.__init__("Thursday", "I am tired")
+    result = diaryentry.format()
+    assert result == "Thursday: I am tired"
+    #f"{self.title}: {contents}"
