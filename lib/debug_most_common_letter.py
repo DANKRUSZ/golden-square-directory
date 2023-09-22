44acc2e11ebbc3e6c8e3195857d6ec01b94f6156
Daniel <dankrusz@hotmail.co.uk>
2023-09-22 13:39:10 +0100

    Added most_common_debug

--- lib/debug_most_common_letter.py
+++ lib/debug_most_common_letter.py
@@ -3,7 +3,7 @@ def get_most_common_letter(text):
     counter = {}
     for char in text.replace(" ", ""):
         counter[char] = counter.get(char, 0) + 1
-    letter =  sorted(counter.items(), key=lambda item: item[1])[-1][0]
+    letter =  sorted(counter.items(), key=lambda item: item[1])[-1][0]#Used discovery to debug this, i know what i changed but cannot explain it
     return letter
 
 
