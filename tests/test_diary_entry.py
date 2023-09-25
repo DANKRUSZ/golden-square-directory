a1c84aab565d7be33c92f2971d694c8b2e59dda3
Daniel <dankrusz@hotmail.co.uk>
2023-09-25 11:03:47 +0100

    Monday morning

--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -7,3 +7,29 @@ def test_format_is_correct():
     result = diaryentry.format()
     assert result == "Thursday: I am tired"
     #f"{self.title}: {contents}"
+
+def test_count_words_correct():
+    diaryentry = DiaryEntry("Today", "I went to the shops")
+    result = diaryentry.count_words()
+    assert result == 6
+
+def test_count_words_long_correct():
+    contents = " ".join(["word" for i in range(0, 100)])
+    diaryentry = DiaryEntry("Today", contents)
+    result = diaryentry.count_words()
+    assert result == 101
+
+def test_reading_time_correct():
+    contents = " ".join(["word" for i in range(0, 100)])
+    diaryentry = DiaryEntry("Friday", contents)
+    result = diaryentry.reading_time(20)
+    assert result == 5
+
+def test_reading_time_correct_long():
+    contents = " ".join(["word" for i in range(0, 1000)])
+    diaryentry = DiaryEntry("Friday", contents)
+    result = diaryentry.reading_time(100)
+    assert result == 10
+
+def test_reading_chunk_correct():
+    pass
