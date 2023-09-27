3c6959eea9632de647c9ff1b894b1cdce665cd08
Daniel <dankrusz@hotmail.co.uk>
2023-09-27 11:55:04 +0100

    Wednesday 11:55am

--- lib/to_do_tracker.py
+++ lib/to_do_tracker.py
@@ -3,20 +3,30 @@
 class TaskTracker():
 
     def __init__(self):
-        # Fields: to do list
+        self.todo_list = []
         pass
 
     def add(self, task):
         # Parameters: string - task
         #adding a task to a to do list
-        pass
+        self.todo_list.append(task)
 
     def return_task_list(self):
         #return my to do list of tasks added in def add
-        pass
+        return self.todo_list
+        
 
     def remove_completed_tasks(self, task):
         #Parameters: task - a task I have completed
         #remove tasks I have completed from my to do list
         # Raise an exception if the task entered is not in list
-        pass
+        for item in self.todo_list:
+            if item == task:
+                self.todo_list.remove(task)
+            else:
+                raise Exception("Task not in to do list")
+        return self.todo_list
+    
+
+
+
--- tests/test_to_do_tracker.py
+++ tests/test_to_do_tracker.py
@@ -1,3 +1,45 @@
+import pytest 
+from lib.to_do_tracker import TaskTracker
 
+# Test to check tasks are added to list
 
-# Test to check tasks are added
+def test_add_task_to_list():
+    tasktracker = TaskTracker()
+    tasktracker.add("Tidy my room")
+    result = tasktracker.return_task_list()
+    assert result == ["Tidy my room"]
+
+def test_add_multiple_tasks():
+    tasktracker = TaskTracker()
+    tasktracker.add("Tidy my room")
+    tasktracker.add("Clean the kitchen")
+    tasktracker.add("Take the bins out")
+    tasktracker.add("Go to the gym")
+    result = tasktracker.return_task_list()
+    assert result == ["Tidy my room", "Clean the kitchen", 
+    "Take the bins out", "Go to the gym"]
+
+def test_tasks_removed():
+    tasktracker = TaskTracker()
+    tasktracker.add("Tidy my room")
+    tasktracker.add("Clean the kitchen")
+    result =tasktracker.remove_completed_tasks("Tidy my room")
+    assert result == ["Clean the kitchen"]
+
+def test_multiple_taks_removed():
+    tasktracker = TaskTracker()
+    tasktracker.add("Tidy my room")
+    tasktracker.add("Clean the kitchen")
+    tasktracker.add("Take the bins out")
+    tasktracker.add("Go to the gym")
+    result = tasktracker.remove_completed_tasks("Take the bins out")
+    result = tasktracker.remove_completed_tasks("Go to the gym")
+    assert result == ["Tidy my room", "Clean the kitchen"]
+
+def test_task_not_in_list():
+    tasktracker = TaskTracker()
+    tasktracker.add("Tidy my room")
+    with pytest.raises(Exception) as e:
+        tasktracker.remove_completed_tasks("Clean the kitchen")
+    error_message = str(e.value)
+    assert error_message == "Task not in to do list"
